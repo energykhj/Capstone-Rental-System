b764728f5fad60c33d02b44ec76e03b229c9e70f
heejin <heuijin.ko@gmail.com>
2021-02-07 10:40:43 -0500

    create lookup controller

---
+++ Server/Controllers/LookupController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LookupController : ControllerBase
+    {
+        private readonly PhoenixContext context;
+        private readonly IMapper mapper;
+
+        public LookupController(PhoenixContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Province>>> GetProvinces()
+        {
+            return await context.Province.ToListAsync();
+        }
+    }
+}
