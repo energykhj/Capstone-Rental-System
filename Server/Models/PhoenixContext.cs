655dbf10c5832077943848cee801c070a254d8e8
heejin <heuijin.ko@gmail.com>
2021-02-18 11:02:00 -0500

    Models - HKo

--- Server/Models/PhoenixContext.cs
+++ Server/Models/PhoenixContext.cs
@@ -15,6 +15,8 @@ namespace Server.Models
         {
         }
 
+        public virtual DbSet<Address> Address { get; set; }
+        public virtual DbSet<AskBoard> AskBoard { get; set; }
         public virtual DbSet<AspNetRoleClaims> AspNetRoleClaims { get; set; }
         public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
         public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
@@ -22,9 +24,16 @@ namespace Server.Models
         public virtual DbSet<AspNetUserRoles> AspNetUserRoles { get; set; }
         public virtual DbSet<AspNetUserTokens> AspNetUserTokens { get; set; }
         public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Item> Item { get; set; }
+        public virtual DbSet<Photo> Photo { get; set; }
         public virtual DbSet<Province> Province { get; set; }
+        public virtual DbSet<RecordStatus> RecordStatus { get; set; }
+        public virtual DbSet<Review> Review { get; set; }
+        public virtual DbSet<Transaction> Transaction { get; set; }
+        public virtual DbSet<TransactionDetail> TransactionDetail { get; set; }
+        public virtual DbSet<TransactionStatus> TransactionStatus { get; set; }
         public virtual DbSet<UserDetails> UserDetails { get; set; }
-        public virtual DbSet<UserStatus> UserStatus { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -37,6 +46,89 @@ namespace Server.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Address1)
+                    .IsRequired()
+                    .HasColumnName("address1")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.Address2)
+                    .HasColumnName("address2")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasColumnName("city")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.IsDefault).HasColumnName("isDefault");
+
+                entity.Property(e => e.PostalCode)
+                    .IsRequired()
+                    .HasColumnName("postalCode")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.ProvinceId).HasColumnName("provinceID");
+
+                entity.Property(e => e.UserId)
+                    .IsRequired()
+                    .HasColumnName("userID")
+                    .HasMaxLength(450);
+
+                entity.HasOne(d => d.Province)
+                    .WithMany(p => p.Address)
+                    .HasForeignKey(d => d.ProvinceId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Address_Province");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Address)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Address_UserDetailes");
+            });
+
+            modelBuilder.Entity<AskBoard>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Date)
+                    .HasColumnName("date")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasColumnName("description")
+                    .HasMaxLength(1024);
+
+                entity.Property(e => e.ParentId).HasColumnName("parentID");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnName("title")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.UserId)
+                    .IsRequired()
+                    .HasColumnName("userID")
+                    .HasMaxLength(450);
+
+                entity.HasOne(d => d.Parent)
+                    .WithMany(p => p.InverseParent)
+                    .HasForeignKey(d => d.ParentId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_AskBoard_AskBoard");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AskBoard)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_AskBoard_UserDetails");
+            });
+
             modelBuilder.Entity<AspNetRoleClaims>(entity =>
             {
                 entity.HasIndex(e => e.RoleId);
@@ -127,63 +219,269 @@ namespace Server.Models
                 entity.Property(e => e.UserName).HasMaxLength(256);
             });
 
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CategoryId).HasColumnName("categoryID");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.AddressId).HasColumnName("addressID");
+
+                entity.Property(e => e.CategoryId).HasColumnName("categoryID");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("createdDate")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Deposit).HasColumnName("deposit");
+
+                entity.Property(e => e.Description)
+                    .HasColumnName("description")
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.EndDate)
+                    .HasColumnName("endDate")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Fee).HasColumnName("fee");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.StartDate)
+                    .HasColumnName("startDate")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.StatusId).HasColumnName("statusID");
+
+                entity.Property(e => e.TimeStamp)
+                    .HasColumnName("timeStamp")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.UserId)
+                    .IsRequired()
+                    .HasColumnName("userID")
+                    .HasMaxLength(450);
+
+                entity.HasOne(d => d.Address)
+                    .WithMany(p => p.Item)
+                    .HasForeignKey(d => d.AddressId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Item_Address");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Item)
+                    .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Item_Category");
+
+                entity.HasOne(d => d.RecordStatus)
+                    .WithMany(p => p.Item)
+                    .HasForeignKey(d => d.StatusId)
+                    .HasConstraintName("FK_Item_RecordStatus");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Item)
+                    .HasForeignKey(d => d.UserId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Item_UserDetails");
+            });
+
+            modelBuilder.Entity<Photo>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.FileName)
+                    .IsRequired()
+                    .HasColumnName("fileName")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.ItemId).HasColumnName("itemID");
+
+                entity.HasOne(d => d.Item)
+                    .WithMany(p => p.Photo)
+                    .HasForeignKey(d => d.ItemId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Photo_Item");
+            });
+
             modelBuilder.Entity<Province>(entity =>
             {
-                entity.Property(e => e.Id).HasColumnName("id");
+                entity.Property(e => e.Id).HasColumnName("ID");
 
-                entity.Property(e => e.Code).HasMaxLength(10);
+                entity.Property(e => e.Code)
+                    .IsRequired()
+                    .HasColumnName("code")
+                    .HasMaxLength(10);
 
-                entity.Property(e => e.Name).HasMaxLength(256);
+                entity.Property(e => e.CountryId).HasColumnName("countryID");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(255);
+            });
+
+            modelBuilder.Entity<RecordStatus>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasColumnName("status")
+                    .HasMaxLength(10);
+            });
+
+            modelBuilder.Entity<Review>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.ItemId).HasColumnName("itemID");
+
+                entity.Property(e => e.Rate).HasColumnName("rate");
+
+                entity.Property(e => e.Review1)
+                    .HasColumnName("review")
+                    .HasMaxLength(1024);
+
+                entity.Property(e => e.Title)
+                    .HasColumnName("title")
+                    .HasMaxLength(255);
+
+                entity.HasOne(d => d.Item)
+                    .WithMany(p => p.Review)
+                    .HasForeignKey(d => d.ItemId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Review_Item");
+            });
+
+            modelBuilder.Entity<Transaction>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.BorrowerId)
+                    .IsRequired()
+                    .HasColumnName("borrowerID")
+                    .HasMaxLength(450);
+
+                entity.Property(e => e.EndDate)
+                    .HasColumnName("endDate")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.ItemId).HasColumnName("itemID");
+
+                entity.Property(e => e.RefundDeposit).HasColumnName("refundDeposit");
+
+                entity.Property(e => e.StartDate)
+                    .HasColumnName("startDate")
+                    .HasColumnType("datetime");
+
+                entity.HasOne(d => d.Borrower)
+                    .WithMany(p => p.Transaction)
+                    .HasForeignKey(d => d.BorrowerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_T_UerDetails");
+
+                entity.HasOne(d => d.Item)
+                    .WithMany(p => p.Transaction)
+                    .HasForeignKey(d => d.ItemId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_T_Item");
+            });
+
+            modelBuilder.Entity<TransactionDetail>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Date)
+                    .HasColumnName("date")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Reason)
+                    .HasColumnName("reason")
+                    .HasMaxLength(255);
+
+                entity.Property(e => e.StatusId).HasColumnName("statusID");
+
+                entity.Property(e => e.TransactionId).HasColumnName("transactionID");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.TransactionDetail)
+                    .HasForeignKey(d => d.StatusId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_TD_TS");
+
+                entity.HasOne(d => d.Transaction)
+                    .WithMany(p => p.TransactionDetail)
+                    .HasForeignKey(d => d.TransactionId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_TD_T");
+            });
+
+            modelBuilder.Entity<TransactionStatus>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasColumnName("status")
+                    .HasMaxLength(50);
             });
 
             modelBuilder.Entity<UserDetails>(entity =>
             {
-                entity.Property(e => e.Id).HasColumnName("id");
+                entity.Property(e => e.Id).HasColumnName("ID");
 
-                entity.Property(e => e.Address1).HasMaxLength(255);
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnName("createdDate")
+                    .HasColumnType("datetime");
 
-                entity.Property(e => e.Address2).HasMaxLength(255);
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasColumnName("firstName")
+                    .HasMaxLength(20);
 
-                entity.Property(e => e.City).HasMaxLength(50);
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasColumnName("lastName")
+                    .HasMaxLength(20);
 
-                entity.Property(e => e.CreateDate).HasColumnType("datetime");
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasColumnName("phone")
+                    .HasMaxLength(12);
 
-                entity.Property(e => e.FirstName).HasMaxLength(255);
+                entity.Property(e => e.PhotoUrl)
+                    .HasColumnName("photoURL")
+                    .HasMaxLength(255);
 
-                entity.Property(e => e.LastName).HasMaxLength(255);
+                entity.Property(e => e.StatusId).HasColumnName("statusID");
 
-                entity.Property(e => e.MiddleNames).HasMaxLength(255);
+                entity.Property(e => e.TimeStamp)
+                    .HasColumnName("timeStamp")
+                    .HasColumnType("datetime");
 
-                entity.Property(e => e.Phone).HasMaxLength(255);
-
-                entity.Property(e => e.PhotoUrl).HasColumnName("PhotoURL");
-
-                entity.Property(e => e.PostalCode).HasMaxLength(6);
-
-                entity.Property(e => e.TimeStamp).HasColumnType("datetime");
-
-                entity.HasOne(d => d.IdNavigation)
+                entity.HasOne(d => d.LoginUser)
                     .WithOne(p => p.UserDetails)
                     .HasForeignKey<UserDetails>(d => d.Id)
                     .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_UserDetails_AspNetUsers");
 
-                entity.HasOne(d => d.Province)
-                    .WithMany(p => p.UserDetails)
-                    .HasForeignKey(d => d.ProvinceId)
-                    .HasConstraintName("FK_UserDetails_Province");
-
-                entity.HasOne(d => d.Status)
+                entity.HasOne(d => d.RecordStatus)
                     .WithMany(p => p.UserDetails)
                     .HasForeignKey(d => d.StatusId)
-                    .HasConstraintName("FK_UserDetails_UserStatus");
-            });
-
-            modelBuilder.Entity<UserStatus>(entity =>
-            {
-                entity.Property(e => e.Id).HasColumnName("id");
-
-                entity.Property(e => e.Status).HasMaxLength(255);
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_UserDetail1_RecordStatus");
             });
 
             OnModelCreatingPartial(modelBuilder);
--- Server/Models/UserDetails.cs
+++ Server/Models/UserDetails.cs
@@ -5,24 +5,28 @@ namespace Server.Models
 {
     public partial class UserDetails
     {
+        public UserDetails()
+        {
+            Address = new HashSet<Address>();
+            AskBoard = new HashSet<AskBoard>();
+            Item = new HashSet<Item>();
+            Transaction = new HashSet<Transaction>();
+        }
+
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string MiddleNames { get; set; }
         public string PhotoUrl { get; set; }
         public string Phone { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string City { get; set; }
-        public string PostalCode { get; set; }
-        public int? ProvinceId { get; set; }
-        public int? CountryId { get; set; }
-        public DateTime? CreateDate { get; set; }
+        public int StatusId { get; set; }
+        public DateTime? CreatedDate { get; set; }
         public DateTime? TimeStamp { get; set; }
-        public int? StatusId { get; set; }
 
-        public virtual AspNetUsers IdNavigation { get; set; }
-        public virtual Province Province { get; set; }
-        public virtual UserStatus Status { get; set; }
+        public virtual AspNetUsers LoginUser { get; set; }
+        public virtual RecordStatus RecordStatus { get; set; }
+        public virtual ICollection<Address> Address { get; set; }
+        public virtual ICollection<AskBoard> AskBoard { get; set; }
+        public virtual ICollection<Item> Item { get; set; }
+        public virtual ICollection<Transaction> Transaction { get; set; }
     }
 }
