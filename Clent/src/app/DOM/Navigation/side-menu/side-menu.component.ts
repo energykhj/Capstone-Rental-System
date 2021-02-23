1a10024ff4f2e951c71746e9b14fa0a8df429991
heejin <heuijin.ko@gmail.com>
2021-02-23 16:06:24 -0500

    UI Search, side menu

--- Clent/src/app/DOM/Navigation/side-menu/side-menu.component.ts
+++ Clent/src/app/DOM/Navigation/side-menu/side-menu.component.ts
@@ -1,5 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { SharedService } from 'src/app/Services/shared.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-side-menu',
@@ -8,15 +10,31 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class SideMenuComponent implements OnInit {
   value = '';
-
+  userDetails: any=[];
+  photoUrl: string;
+  userName: string="";
   @Output() sidenavClose = new EventEmitter();
 
   constructor(
     private router: Router,
-  ) { }
+    private service: SharedService,
+  ) { this.getUser();}
 
+  getUser(){
+    const userId: string = localStorage.getItem("userId");    
+    if(userId){
+      this.service.GetUserInfo.subscribe(user =>{
+        this.userDetails = user.details;        
+        this.userName = (this.userDetails.firstName)? this.userDetails.firstName +" "+ this.userDetails.lastName : "";
+        this.photoUrl = (this.userDetails.photoUrl)? environment.PhotoFileUrl+this.userDetails.photoUrl : "";
+        
+        // alert(this.photoUrl); 
+      });
+    } 
+  }
   ngOnInit(): void {
   }
+
 
   public onSidenavClose = () => {
     this.sidenavClose.emit();
