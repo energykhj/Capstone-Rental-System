88e7e2b250f1881057ed88233219e7dfa61a8702
Younghee <jyhstar@gmail.com>
2021-02-24 21:40:59 -0500

    login unit test

--- Clent/src/app/DOM/Account/login/login.component.spec.ts
+++ Clent/src/app/DOM/Account/login/login.component.spec.ts
@@ -1,6 +1,11 @@
+import 'zone.js/dist/zone-testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
+import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +13,15 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports:[RouterTestingModule.withRoutes([]),
+              HttpClientTestingModule,
+              ReactiveFormsModule,
+              MatDialogModule,
+              FormsModule],
+      providers: [{provide: MatDialogRef,useValue:{}},
+                {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue:{hasBackdrop: false}}],
+      schemas:      [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   });
