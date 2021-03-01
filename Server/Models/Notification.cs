c557a24abec4bbddef1972c63ce50bf6d19c9291
heejin <heuijin.ko@gmail.com>
2021-03-01 10:43:07 -0500

    Notification - model

---
+++ Server/Models/Notification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Server.Models
+{
+    public partial class Notification
+    {
+        public int Id { get; set; }
+        public string FromUserId { get; set; }
+        public string ToUserId { get; set; }
+        public int? ItemId { get; set; }
+        public int? NotiType { get; set; }
+        public string Message { get; set; }
+        public DateTime? SendDate { get; set; }
+
+        public virtual UserDetails FromUser { get; set; }
+        public virtual Item Item { get; set; }
+        public virtual NotificationType NotiTypeNavigation { get; set; }
+        public virtual UserDetails ToUser { get; set; }
+    }
+}
---
+++ Server/Models/NotificationType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Server.Models
+{
+    public partial class NotificationType
+    {
+        public NotificationType()
+        {
+            Notification = new HashSet<Notification>();
+        }
+
+        public int Id { get; set; }
+        public string Type { get; set; }
+
+        public virtual ICollection<Notification> Notification { get; set; }
+    }
+}
