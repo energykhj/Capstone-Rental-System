fa3e797bd91d49fb84a136dcb124a382dc569f4f
Younghee <jyhstar@gmail.com>
2021-03-13 20:47:49 -0500

    my-list no image add

--- Client/src/app/DOM/Myspace/my-list/my-list.component.spec.ts
+++ Client/src/app/DOM/Myspace/my-list/my-list.component.spec.ts
@@ -1,4 +1,10 @@
+import 'zone.js/dist/zone-testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthService } from './../../../Services/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 
 import { MyListComponent } from './my-list.component';
 
@@ -8,9 +14,14 @@ describe('MyListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MyListComponent ]
-    })
-    .compileComponents();
+      declarations: [MyListComponent],
+      providers: [
+        { provide: AuthService },
+        { provide: MatDialog, useValue: {} },
+        { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+        JwtHelperService,
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
