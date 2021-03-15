22c18d048eb82691d42e8d56f13e5b1695d50a19
heejin <heuijin.ko@gmail.com>
2021-03-15 17:10:00 -0400

    Add Notification

---
+++ Server/BizLogic/NotificationBiz.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.BizLogic
+{
+    public partial class NotificationBiz
+    {
+        private readonly PhoenixContext context;
+
+        List<int> errorList = new List<int>();
+        Notification notification = new Notification();
+
+        public NotificationBiz(PhoenixContext _context)
+        {
+            this.context = _context;
+        }
+
+        public async Task<List<Notification>> GetNotification(string userId)
+        {
+            return await context.Notification
+                .Where((c => c.FromUserId == userId || 
+                        c.ToUserId == userId) )
+                .ToListAsync();
+        }
+    }
+}
---
+++ Server/Controllers/NotificationController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Server.BizLogic;
+using Server.Models;
+using Shared.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly PhoenixContext context;
+        private readonly IMapper mapper;
+        private readonly NotificationBiz NB;
+        private readonly UserBiz UB;
+
+        public NotificationController(PhoenixContext _context, IMapper _mapper)
+        {
+            this.context = _context;
+            this.mapper = _mapper;
+            NB = new NotificationBiz(context);
+            UB = new UserBiz(context);
+        }
+
+        [HttpGet("GetTransactionByUser")]
+        public async Task<ActionResult<List<NotificationDTO>>> GetNotification(string userId, DateTime startDate)
+        {         
+            var noti = mapper.Map<List<NotificationDTO>>(await NB.GetNotification(userId/*, startDate*/));
+            return noti;
+        }
+
+        [HttpPost("InsertNotification")]
+        public async Task<ActionResult<List<NotificationDTO>>> InsertNotification(NotificationDTO dto)
+        {
+
+            var noti = mapper.Map<List<NotificationDTO>>(await NB.GetNotification("dd"));
+            return noti;
+        }
+
+        [HttpPut("UpdateNotificationStatus")]
+        public async Task<ActionResult<List<NotificationDTO>>> UpdateNotificationStatus(int notiId)
+        {
+
+            var noti = mapper.Map<List<NotificationDTO>>(await NB.GetNotification("dd"));
+            return noti;
+        }
+    }
+
+
+}
