3e674ac42a503c66f207c933e799a4c2a97bb552
yhong <hyp0126@gmail.com>
2021-03-15 12:16:14 -0400

    fix test.ts

--- Client/src/app/DOM/Account/login/login.component.spec.ts
+++ Client/src/app/DOM/Account/login/login.component.spec.ts
@@ -1,9 +1,15 @@
-import 'src/app/DOM/Main/Post/add-edit-post/node_modules/zone.js/dist/zone-testing';
+import 'zone.js/dist/zone-testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
-import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+  MatDialog,
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MAT_DIALOG_DEFAULT_OPTIONS,
+} from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
@@ -13,17 +19,20 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ],
-      imports:[RouterTestingModule.withRoutes([]),
-              HttpClientTestingModule,
-              ReactiveFormsModule,
-              MatDialogModule,
-              FormsModule],
-      providers: [{provide: MatDialogRef,useValue:{}},
-                {provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue:{hasBackdrop: false}}],
-      schemas:      [ NO_ERRORS_SCHEMA ]
-    })
-    .compileComponents();
+      declarations: [LoginComponent],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+        FormsModule,
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DEFAULT_OPTIONS, useValue: { hasBackdrop: false } },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
