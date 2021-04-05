5ee3c161a930dbe14691ce1aafbb65bb4fa74e1b
yhong <hyp0126@gmail.com>
2021-04-05 17:18:35 -0400

    Add Notification Badge in sidemenu (my space)

--- Client/src/app/DOM/Navigation/side-menu/side-menu.component.ts
+++ Client/src/app/DOM/Navigation/side-menu/side-menu.component.ts
@@ -5,6 +5,7 @@ import { environment } from 'src/environments/environment';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from 'src/app/DOM/Account/login/login.component';
 import { HeaderComponent } from 'src/app/DOM/Navigation/header/header.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-side-menu',
@@ -15,28 +16,42 @@ export class SideMenuComponent implements OnInit {
   @Output() sidenavClose = new EventEmitter();
 
   search = '';
+  userAccount: any = [];
   userDetails: any = [];
   photoUrl: string;
   userName: string = '';
   selectedCity: string = HeaderComponent.ALL_CITIES;
   cityList: any = [];
+  notificationCount = 0;
+  subscription: Subscription;
 
   constructor(private router: Router, private service: SharedService, public dialog: MatDialog) {
     this.getUser();
+    // subscribe Notification Message Count
+    this.subscription = this.service.getNotificationCount().subscribe((count) => {
+      if (count) {
+        this.notificationCount = count;
+      } else {
+        this.notificationCount = 0;
+      }
+    });
   }
 
   getUser() {
     const userId: string = localStorage.getItem('userId');
     if (userId) {
       this.service.getUserInfo.subscribe((user) => {
+        this.userAccount = user.account;
         this.userDetails = user.details;
         this.userName = this.userDetails.firstName ? this.userDetails.firstName + ' ' + this.userDetails.lastName : '';
         this.photoUrl = this.userDetails.photoUrl ? environment.PhotoFileUrl + this.userDetails.photoUrl : '';
 
         // alert(this.photoUrl);
+        this.getNotificationCount();
       });
     }
   }
+
   ngOnInit(): void {
     this.getCityList();
   }
@@ -48,6 +63,18 @@ export class SideMenuComponent implements OnInit {
         this.cityList.push(data[i]);
       }
       this.cityList.push(HeaderComponent.ALL_CITIES);
+    });
+  }
+
+  getNotificationCount() {
+    var startDate = new Date(new Date().setDate(new Date().getDate() - 30));
+    var startDateStr = startDate.toUTCString();
+    this.service.getNotification(this.userAccount.id, startDateStr).subscribe((notifications: any) => {
+      var filterdNotification = notifications.filter((el) => {
+        return el.toUserId == this.userAccount.id;
+      });
+      //this.notificationCount = filterdNotification.length;
+      this.service.sendNotificationCount(filterdNotification.length);
     });
   }
 
